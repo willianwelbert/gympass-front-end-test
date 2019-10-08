0c6f7747b03cfd7ce1d5b0339bbc253fe781cc13
Willian <willianwelbert@yahoo.com.br>
2019-10-08 17:05:14 -0300

    Updates storybook welcome and fixes unused import in footer story

--- src/stories/0-Welcome.stories.js
+++ src/stories/0-Welcome.stories.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { linkTo } from '@storybook/addon-links';
-import { Welcome } from '@storybook/react/demo';
+import styled from 'styled-components';
+
+const WelcomeContainer = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  width: 80%;
+  margin: 0 auto;
+  line-height: 2rem;
+`
 
 export default {
-  title: 'Welcome',
+  title: 'Gympass',
 };
 
-export const toStorybook = () => <Welcome showApp={linkTo('Button')} />;
+export const toStorybook = () => 
+  <WelcomeContainer>
+    <h1 style={{color: '#FF5A49'}}>Gympass Front End Test Storybook</h1>
+    <h5 style={{textAlign: 'right'}} >Willian Welbert</h5>
+    <p>Este Storybook (o meu primeiro), agrupa os componentes desenvolvidos para o teste de Front End do Gympass.</p>
+    <p>Estão ordenados por ordem de complexidade e/ou quantidade de props relacionadas. Essa ordem foi escolhida apenas por facilitar meu processo de desenvolvimento.</p>
+    <p>Feedbacks são bem-vindos!</p>
+    <p><a href='https://linkedin.com/in/willianwelbert' target='_blank' rel='noreferrer noopener'>LinkedIn</a> | <a href='mailto:willianwelbert@yahoo.com.br'>Email</a></p>
+  </WelcomeContainer>;
 
 toStorybook.story = {
-  name: 'to Storybook',
+  name: 'Front End Test',
 };
--- src/stories/1-Footer.stories.js
+++ src/stories/1-Footer.stories.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Footer from '../components/Footer/footer.component';
 
-import {FooterContainer, FooterBrandLine, FooterCopy} from '../components/Footer/footer.styles.jsx';
-
 export default {
   component: Footer,
   title: 'General Footer',
