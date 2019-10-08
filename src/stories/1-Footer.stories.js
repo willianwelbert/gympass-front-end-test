9d859b60cb8bb1ba466d9fdf0caa5fe18b06acc8
Willian <willianwelbert@yahoo.com.br>
2019-10-08 14:26:35 -0300

    cleans up footer story

--- src/stories/1-Footer.stories.js
+++ src/stories/1-Footer.stories.js
@@ -8,18 +8,5 @@ export default {
   title: 'General Footer',
 };
 
-export const basic = () => 
-  <FooterContainer >
-    <FooterCopy>
-      made with <span role='img' aria-label='brain emoji'>🧠</span>, <span role='img' aria-label='flexing arm emoji'>💪</span> and <span role='img' aria-label='red heart emoji'>❤️</span> by <a href='https://github.com/willianwelbert' target='_blank' rel='noreferrer noopener'>willian welbert</a>
-    </FooterCopy>
-  </FooterContainer>
-
-
 export const gympassBranded = () => 
-  <FooterContainer >
-    <FooterBrandLine/>
-    <FooterCopy>
-      made with <span role='img' aria-label='brain emoji'>🧠</span>, <span role='img' aria-label='flexing arm emoji'>💪</span> and <span role='img' aria-label='red heart emoji'>❤️</span> by <a style={{color: '#FF5A49'}} href='https://github.com/willianwelbert' target='_blank' rel='noreferrer noopener'>willian welbert</a>
-    </FooterCopy>
-  </FooterContainer>
+  <Footer />
