b7d1ca95a7a418cd102b1ebd4f4b78bba1b03169
Willian <willianwelbert@yahoo.com.br>
2019-10-10 11:24:38 -0300

    style tweeks on Error component and cleans unused import in Loader story

--- src/stories/2-Loader.stories.js
+++ src/stories/2-Loader.stories.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Loader from '../components/Loader/Loader.component.jsx';
-import ErrorMessage from '../components/Error/Error.component.jsx';
 
 export default {
   component: Loader,
