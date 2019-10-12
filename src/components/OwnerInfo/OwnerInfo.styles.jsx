6659b3c6a291a767d8bbe072e3672b0ccd05dd55
Willian Welbert <willianwelbert@yahoo.com.br>
2019-10-12 17:41:09 -0300

    tweeks owner info/header styles to position at top

--- src/components/OwnerInfo/OwnerInfo.styles.jsx
+++ src/components/OwnerInfo/OwnerInfo.styles.jsx
@@ -2,14 +2,17 @@ import styled from 'styled-components';
 
 export const InfoContainer = styled.div`
   display: flex;
+  position: fixed;
+  top: 0;
   width: 100%;
+  padding: 1rem;
   background-color: #fcfcfc;
   border-bottom: 2px solid #eee;
   justify-content: space-around;
   align-items: center;
   font-family: 'Open Sans', sans-serif;
   color: #555;
-`
+`;
 
 export const Info = styled.span`
   font-size: 1rem;
@@ -23,17 +26,17 @@ export const Caption = styled.span`
   margin: 0;
   padding: 0.2rem 0;
   display: block;
-`
+`;
 
 export const OwnerAvatar = styled.div`
   width: 5rem;
   height: 5rem;
   max-width: 300px;
   border-radius: 50%;
-  border: 3px solid #FF5A49;
-  margin-bottom: -1rem;
+  border: 3px solid #ff5a49;
+  margin-bottom: -3rem;
   background-size: 110%;
   background-position: center;
-`
+`;
 
-export const OwnerRepoCount = styled.span``
+export const OwnerRepoCount = styled.span``;
