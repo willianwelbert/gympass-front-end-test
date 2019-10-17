82267b5809f2f0d14b1731e43ed8c8e83151745a
Willian <willianwelbert@yahoo.com.br>
2019-10-16 21:28:56 -0300

    adds fallback description to commits without author or message

--- src/components/List/List.component.jsx
+++ src/components/List/List.component.jsx
@@ -4,27 +4,27 @@ import SearchBar from '../SearchBar/SearchBar.component';
 import ListItem from '../ListItem/ListItem.component';
 import PropTypes from 'prop-types';
 
-const List = (props) => {
-  const { listData } = props;
+const List = ({ listData }) => {
+
   return(
     <ListContainer>
       <SearchBar />
       {listData.map( (item, idx) => {
-        const { name, language, clone_url, stargazers_count, description } = item;
+        const { name, language, clone_url, stargazers_count, description, } = item;
         
         return clone_url 
           ? <ListItem 
-            key={idx} 
-            description={description} 
-            programmingLanguage={language} 
-            title={name} 
-            itemType='repo'
-            iconInfo={stargazers_count}
+              key={idx} 
+              description={description} 
+              programmingLanguage={language} 
+              title={name} 
+              itemType='repo'
+              iconInfo={stargazers_count}
             /> 
           : <ListItem 
-          key={idx}
-          description={item.commit.message}
-          iconInfo={item.author.login}
+              key={idx}
+              description={item.commit ? item.commit.message : 'no commit message'}
+              iconInfo={item.author ? item.author.login : 'no commit author'}
           />;
 
       } )}
